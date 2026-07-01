ceca32589a983d4332c606e7f62dd1493eddc082
zhaochenyou <linrongbin16@gmail.com>
2026-07-01 23:13:54 +0800

    c

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -42,7 +42,7 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/mivsr/mivsr.github.io/tree/main/",
+          editUrl: "https://github.com/mivsr/rsvim.github.io/tree/main/",
         },
         theme: {
           customCss: "./src/css/custom.css",
